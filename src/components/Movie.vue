f6fa864e295f39b7f34960834702c6be739932f2
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-08-20 18:05:28 +0300

    feature: create Movie component

---
+++ src/components/Movie.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>{{ movieData.name }}</h3>
+    <p>{{ movieData.year }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Movie',
+  props: {
+    movieData: {
+      name: String,
+      year: Number,
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
