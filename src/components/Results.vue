19e6eac691b13b58a14417c29238bf5243c40fc5
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-08-20 18:04:59 +0300

    feature: create Result component

---
+++ src/components/Results.vue
@@ -0,0 +1,43 @@
+<template>
+  <ol class="results">
+    <li :key="i" v-for="(mData, i) of movies">
+      <Movie :movieData="mData" />
+      <div class="boundary"></div>
+    </li>
+  </ol>
+</template>
+
+<script>
+import Movie from './Movie.vue';
+
+export default {
+  name: 'Results',
+  components: {
+    Movie,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    movies() {
+      return this.$store.getters.MOVIES || []
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .results {
+    border: 1px solid #777;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .boundary {
+    width: 100%;
+    height: .2rem;
+    background-color: #999;
+  }
+</style>
